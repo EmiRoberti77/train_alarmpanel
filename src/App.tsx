a8639968e6dbbb50eeeafb79b4a02a5f339c9355
Reinhard kenson <kenstudios@Reinhards-MacBook-Pro.local>
2023-06-15 17:44:15 +0530

    Bug update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { AlarmsComponent } from './componets/AlarmsComponent';
+import { AlarmsComponentDisplay } from './componets/AlarmsComponentDisplayLight';
 
 function App() {
   return (
     <div className="App">
       <AlarmsComponent />
+      {/* <AlarmsComponentDisplay/> */}
     </div>
   );
   //emi
